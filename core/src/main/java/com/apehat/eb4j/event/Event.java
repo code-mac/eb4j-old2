eb8a1cdfae915aea905c9c4ca980b784a14f55db
code-mac <hpf@apehat.com>
2018-06-10 01:05:38 +0800

    Use EventSource

--- core/src/main/java/com/apehat/eb4j/event/Event.java
+++ core/src/main/java/com/apehat/eb4j/event/Event.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Apehat.com
+ * Copyright (c) 2018 Apehat.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +16,17 @@
 
 package com.apehat.eb4j.event;
 
-import com.apehat.eb4j.source.SourceId;
+import com.apehat.eb4j.source.EventSource;
 import com.apehat.lang.annotation.NotNull;
 
 /**
+ * 事件应该是一个不可变的对象
+ * 但是，现在无法保证的是，事件体不会发生改变。
+ * 包括在事件发布后，发布者不会对事件进行改变。以及订阅者不会对事件进行修改。
+ * <p>
+ * 在这种情况下，想要保证事件的不变性，只能对事件进行深度的拷贝。
+ * 但是，在这种情况下，事件的深度拷贝必定带来性能问题。尤其是面对比较庞大的事件时，对CPU以及内存存储的消耗必定是巨大的
+ *
  * @author hanpengfei
  * @since 1.0
  */
@@ -32,5 +39,5 @@ public interface Event {
     Object body();
 
     @NotNull
-    SourceId source();
+    EventSource source();
 }
